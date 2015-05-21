a03413f8a9e709279d6424526aee80c1ed898245
Si Pham <spham@linkedin.com>
2015-05-21 14:02:09 -0700

    Changed tabs to spaces to look better

--- Main.js
+++ Main.js
@@ -2,9 +2,9 @@ var request = require('request');
 var Airbnb = require('./Airbnb');
 
 Airbnb.search({
-	location: 'New York, NY',
-	checkin: '06/03/2015',
-	checkout: '06/06/2015'
+    location: 'New York, NY',
+    checkin: '06/03/2015',
+    checkout: '06/06/2015'
 }, function(err, res, body) {
-	console.log(body);
+    console.log(body);
 });
