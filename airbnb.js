2a2ab2586af5f013f072c4a1d0fdc158b2fa7b3a
Gerasev Kirill <gerasev.kirill@gmail.com>
2016-07-01 11:34:42 +0300

    [+] api: getListingReviews. more at http://airbnbapi.org/#get-reviews

--- airbnb.js
+++ airbnb.js
@@ -8,6 +8,7 @@
  * - getCalendar() - Get availability for a hosting
  * - getEstIncome() - Get estimate income a hosting generates, by month
  * - getReviews() - Get reviews for a given user, as host or guest
+ * - getListingReviews() - Get reviews for a given listing id
  */
 
 var search = require('./api/search'),
@@ -15,6 +16,7 @@ var search = require('./api/search'),
     getCalendar = require('./api/getCalendar'),
     getReviews = require('./api/getReviews'),
     getEstIncome = require('./api/getEstIncome'),
+    getListingReviews = require('./api/getListingReviews'),
     login = require('./api/login');
 
 var USERNAME = 'username',
@@ -30,6 +32,7 @@ var AirbnbClient = function(optionalConfigs) {
     getCalendar: getCalendar,
     getReviews: getReviews,
     getEstIncome: getEstIncome,
+    getListingReviews: getListingReviews,
     login: login
   };
 };
--- configs/configs.js
+++ configs/configs.js
@@ -2,9 +2,11 @@ var secrets = require('./_secrets'),
     _ = require('lodash');
 
 var AIRBNB_PREFIX = 'https://www.airbnb.com',
+    AIRBNB_API_PREFIX = 'https://api.airbnb.com',
     SEARCH_URL = AIRBNB_PREFIX + '/search/search_results',
     AVAILABILITY_URL = AIRBNB_PREFIX + '/api/v2/calendar_months',
     HOSTING_INFO_URL = AIRBNB_PREFIX + '/api/v1/listings',
+    LISTING_REVIEWS_URL = AIRBNB_API_PREFIX + '/v2/reviews',
     USER_REVIEWS_URL = AIRBNB_PREFIX + '/users/review_page',
     LOGIN_URL = AIRBNB_PREFIX + '/api/v1/authorize',
     DEFAULT_REQUEST_CONFIGS = {
@@ -18,9 +20,11 @@ var AIRBNB_PREFIX = 'https://www.airbnb.com',
 
 module.exports = {
   AIRBNB_PREFIX: AIRBNB_PREFIX,
+  AIRBNB_API_PREFIX: AIRBNB_API_PREFIX,
   SEARCH_URL: SEARCH_URL,
   AVAILABILITY_URL: AVAILABILITY_URL,
   HOSTING_INFO_URL: HOSTING_INFO_URL,
+  LISTING_REVIEWS_URL: LISTING_REVIEWS_URL,
   USER_REVIEWS_URL: USER_REVIEWS_URL,
   LOGIN_URL: LOGIN_URL,
   DEFAULT_REQUEST_CONFIGS: DEFAULT_REQUEST_CONFIGS,
